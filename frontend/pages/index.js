25cecf4ab70b4f8929a6bf3ac13516286b4a84e0
Ivor Padilla <ivorjpc@gmail.com>
2019-01-03 11:19:35 -0400

    removed node_modules from commit

---
+++ frontend/pages/index.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class index extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Hello world</h1>
+      </div>
+    )
+  }
+}
+
+export default index
